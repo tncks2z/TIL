16b2ef42ec6805687c27be7424168185a59edf82
tncks2z <tncks2z@gmail.com>
2022-02-23 15:32:36 +0900

    2/23 15:32

--- JO_937.py
+++ JO_937.py
@@ -1,7 +1,7 @@
 n,m = int(input().split())
 
-num_lst_1 = [list(map(int, input().split())) for i in range(n)]
-num_lst_2 = [list(map(int, input().split())) for i in range(n)]
+num_lst_1 = [list(map(int, input().split())) for _ in range(n)]
+num_lst_2 = [list(map(int, input().split())) for _ in range(n)]
 
 result = [[0]*n for _ in range(m)]
 
